0b9b3c400a3412eb93af2a1736354bed518c6e11
jeff <johnjeff55@gmail.com>
2025-04-12 03:14:15 -0700

    Update zip content file names

--- generateExtensionPackage.js
+++ generateExtensionPackage.js
@@ -7,7 +7,7 @@ const AdmZip = require('adm-zip')
  * @param {string} outputPath - Path where ZIP file should be saved
  * @returns {Promise<string>} Path to the created ZIP file
  */
-async function generateZip(outputPath = './demoZip.zip') {
+async function generateZip(outputPath = './extensionPackage.zip') {
   try {
     const zip = new AdmZip()
     const rootDir = path.resolve(__dirname, '')
